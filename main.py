5b156075f650b550cb266a89385164a3e01b6b3d
Abner Felipe <60849638+AFOFerreira@users.noreply.github.com>
2026-04-27 16:58:45 -0300

    update method

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
-import uuid
-import os
-import subprocess
+from pydantic import BaseModel
+import uuid, subprocess
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "online"}
+class TTSRequest(BaseModel):
+    text: str
 
 @app.post("/tts")
-def tts(text: str = Form(...)):
+def tts(data: TTSRequest):
+
     uid = str(uuid.uuid4())
 
     wav = f"/app/output/{uid}.wav"
     mp3 = f"/app/output/{uid}.mp3"
 
-    cmd1 = f'echo "{text}" | piper --model /app/voices/voz.onnx --output_file {wav}'
-    subprocess.run(cmd1, shell=True)
+    cmd = f'echo "{data.text}" | piper --model /app/voices/voz.onnx --output_file {wav}'
+    subprocess.run(cmd, shell=True)
 
-    cmd2 = f'ffmpeg -y -i {wav} -af "loudnorm,acompressor" {mp3}'
-    subprocess.run(cmd2, shell=True)
+    subprocess.run(
+        f'ffmpeg -y -i {wav} -af "loudnorm,acompressor" {mp3}',
+        shell=True
+    )
 
     return FileResponse(mp3, media_type="audio/mpeg", filename="audio.mp3")
